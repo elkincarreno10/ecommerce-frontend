767a66cabf87ee3d10b810893ac3cb74de05a505
elkincarreno10 <elquin50000@gmail.com>
2026-06-02 19:32:52 -0500

    fix: deploy vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import ProductFiltersProvider from "@/context/ProductFiltersContext";
 import ProductsContent from "@/components/product/ProductsContent";
 
 export default function Home() {
     return (
-        <ProductFiltersProvider>
-            <ProductsContent />
-        </ProductFiltersProvider>
+        <Suspense>
+            <ProductFiltersProvider>
+                <ProductsContent />
+            </ProductFiltersProvider>
+        </Suspense>
     );
 }
